8cc00d37d5bc59d63726cec9fc57d208d06c39c2
kchmz <113977002+kchmz@users.noreply.github.com>
2022-12-13 09:44:36 +0000

    Add files via upload

---
+++ NameAge.py
@@ -0,0 +1,17 @@
+Forename = "Kurtis"
+Surname = "Cousins"
+
+Age = 16
+
+Fullname = Forename + " " + Surname
+
+print("Your first name is " + Forename)
+print("Your Surname is " + Surname)
+
+print("This year you are " + str(Age) + " Years old")
+
+Age +=1
+
+print("Next year you will be " + str(Age) + " years old")
+
+print("end.")
